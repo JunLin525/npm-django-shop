81d191b746004a501829778ed297def6b60974a2
shao <junlin5525@gmail.com>
2023-08-03 15:17:57 +0000

    feat: 🎸 add frontend to github

--- backend/Locate/views.py
+++ backend/Locate/views.py
@@ -1,22 +1,25 @@
 from django.shortcuts import render
 from rest_framework import generics
 from .models import shop, comment
-from .serializers import ShopSerializer,CommentSerializer
+from .serializers import ShopSerializer, CommentSerializer
 # Create your views here.
+
 
 class ShopPostList(generics.ListCreateAPIView):
     queryset = shop.objects.all()
     serializer_class = ShopSerializer
 
+
 class ShopPostDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = shop.objects.all()
     serializer_class = ShopSerializer
+
 
 class CommentPostList(generics.ListAPIView):
     queryset = comment.objects.all()
     serializer_class = CommentSerializer
 
+
 class CommentPostDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = comment.objects.all()
     serializer_class = CommentSerializer
-    
